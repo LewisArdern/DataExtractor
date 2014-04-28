20c4a8c09f855176a0916dfbc3aefaf29d77de2e
LewisArdern <LewisArdern@live.co.uk>
2014-04-28 10:50:02 +0100

    removed static directory

--- savetocsv.rb
+++ savetocsv.rb
@@ -1,11 +1,11 @@
 require 'csv'
-
+ROOT_DIR = File.dirname(__FILE__)
 class Save
 
 	def save_to_file(names)
 		p "saving to file"
 
-		File.open('/Users/lewisardern/Documents/DataCrawl/myfile.csv', 'w') do |line|
+		File.open('#{ROOT_DIR}/myfile.csv', 'w') do |line|
 			line << "firstname, lastname\n"
 
 		names.each do |n|		
